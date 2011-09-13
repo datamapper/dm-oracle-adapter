fdeb9ec6e7e97886cdf2bcdab71dd2fa3bf08a14
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-09-13 11:21:00 +0200

    Making the adapter run with ruby 1.8 without activesupport or stuff like that

--- lib/dm-oracle-adapter/adapter.rb
+++ lib/dm-oracle-adapter/adapter.rb
@@ -102,8 +102,8 @@ module DataMapper
 
           # create subquery to find all valid keys and then use these keys to retrive all other columns
           use_subquery = qualify
-          no_group_by  = group_by.blank?
-          no_order     = order.blank?
+          no_group_by  = group_by.to_s.empty?
+          no_order     = order.to_s.empty?
 
           # when we can include ROWNUM condition in main WHERE clause
           use_simple_rownum_limit = limit && (offset||0 == 0) && no_group_by && no_order
@@ -141,7 +141,7 @@ module DataMapper
           end
           statement << from_statement
           statement << join_statement(query, bind_values, qualify)     if qualify
-          statement << " WHERE (#{conditions_statement})" unless conditions_statement.blank?
+          statement << " WHERE (#{conditions_statement})" unless conditions_statement.to_s.empty?
           if use_subquery
             statement << ")"
           end
